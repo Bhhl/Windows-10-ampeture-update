ffa780f27af966db5f53b08581e68327a6b717bf
Bhhl <51732503+Bhhl@users.noreply.github.com>
2020-01-11 12:34:07 +1000

    Add files via upload

---
+++ windows.c
@@ -0,0 +1,83 @@
+using Windows.c
+unlock Windows;
+unlock Windows_hight_performance;
+unlock Windows_desktop_optimized;
+unlock Windows_hight_quality_screen;
+unlock Windows_hight_definition_screen;
+unlock Windows_screen_resolution = unlimitedheightandweight;
+unlock Windows_reactive_core;
+unlock Windows_reactive_shell;
+unlock Windows_neuron_core;
+unlock Windows_neuron_shell;
+unlock Windows_clear_screen;
+unlock Windows_pixellase_screen;
+unlock Windows_sound_volume = 3D;
+unlock Windows_fps = unlimitedrate;
+unlock Windows_sound_type = 9.1;
+unlock Windows_sound_deep = unlimiteddeep;
+unlock Windows_screen_deep = unlimiteddeep;
+unlock Windows_united_screen_resolution;
+unlock Windows_united_screen_dpi;
+unlock Windows_united_textures_quality;
+unlock Windows_united_textures_definition;
+unlock Windows_united_screen_size;
+unlock Windows_united_textures_size;
+unlock Windows_united_textures_resolition; 
+unlock Windows_united_ray_tracing;
+unlock Windows_rate_booster = science;
+unlovk Windows_framerate_booster = sciense;
+unlock Windows_visual_mode = science;
+unlock Windows_alternate_performance = sciense, int main screen, int computer;
+unlock Windows, int main screen, int computer;
+
+//Windows performance optimized
+int computer;
+void computer; 
+
+//Windows performance booster
+int computer;
+void computer 
+//Windows ultra performance
+int computer;
+void computer;
+//Windows limit resolution = 0 - unlimited weight and height
+int computer;
+void computer;
+//Windows ultra definition screen
+int computer;
+void computer;
+//Windows ultra RED, GREEN, BLUE
+int computer;
+void computer;
+///Windows optimized
+int computer;
+void computer;
+
+///Windows hardware optimized
+int computer;
+void computer;
+
+///Windows network optimized
+int computer;
+void computer;
+
+///Windows network OSI optimized
+int computer;
+void computer;
+
+///Windows network signal size = 59db
+int computer;
+void computer;
+
+///C source file compiled
+int computer;
+void computer;
+
+///Windows tplink speed = 50gb/s, 50gbps
+int computer;
+void computer;
+
+///Windows tplink rate = 100gb/s, 100gbps
+int computer;
+void computer;
+
